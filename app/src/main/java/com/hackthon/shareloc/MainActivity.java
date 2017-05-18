ec578e69eec450e022799274abc024d632532ea7
חנן בן ציון <hananbnz@gmail.com>
2017-05-18 17:02:26 +0300

    job start

--- app/src/main/java/com/hackthon/shareloc/MainActivity.java
+++ app/src/main/java/com/hackthon/shareloc/MainActivity.java
@@ -1,13 +1,118 @@
 package com.hackthon.shareloc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    final List<Photo> photos = new ArrayList<Photo>();
+//    RecyclerView rv;
+    private static class Photo
+    {
+        String id;
+        String title;
+    }
+
+    private static class PhotoVH extends RecyclerView.ViewHolder{
+        ImageView photo;
+        TextView title;
+
+        public PhotoVH(View itemView){
+            super(itemView);
+        }
+    }
+
+    private void render(final List<Photo> photos){
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rv_of_photos);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.Adapter<PhotoVH> adapter = new RecyclerView.Adapter<PhotoVH>(){
+            @Override
+            public PhotoVH onCreateViewHolder(ViewGroup parent, int viewType) {
+                PhotoVH vh = new PhotoVH(getLayoutInflater().inflate(R.layout.item, null));
+                vh.photo = (ImageView) vh.itemView.findViewById(R.id.photo);
+                vh.title = (TextView) vh.itemView.findViewById(R.id.title);
+                return vh;
+            }
+
+            @Override
+            public void onBindViewHolder(PhotoVH holder, int position) {
+                Picasso.with(MainActivity.this).load("https://i.imgur.com/" +
+                        photos.get(position).id + ".jpg").into(holder.photo);
+                holder.title.setText(photos.get(position).title);
+            }
+
+            @Override
+            public int getItemCount() {
+                return photos.size();
+            }
+        };
+
+        rv.setAdapter(adapter);
+
+        rv.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                                       RecyclerView.State state ) {
+                outRect.bottom = 16;
+            }
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                render(photos);
+            }
+        });
+
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds item to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
+
+
+
+
+
+
+
+
+
